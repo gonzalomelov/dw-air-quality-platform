463bb9628c2707f0e4acb12b017922e5ebf109e7
Gonzalo Melo <gonzalo@kona.tech>
2019-06-15 14:10:35 -0300

    initial

---
+++ start.sh
@@ -0,0 +1,35 @@
+cd admin
+git pull origin master
+docker-compose down
+docker-compose up -d
+cd ..
+cd db-service
+git pull origin master
+docker-compose down
+docker-compose up -d
+cd ..
+cd docker-biserver-ce
+git pull origin 8.0
+docker-compose down
+docker-compose up -d
+cd ..
+cd docker-pdi-ce
+git pull origin 8.0
+docker-compose down
+docker-compose up -d
+cd ..
+cd docsify
+git pull origin master
+docker-compose down
+docker-compose up -d
+cd ..
+cd filestash
+git pull origin master
+docker-compose down
+docker-compose up -d
+cd ..
+cd sftp
+git pull origin master
+docker-compose down
+docker-compose up -d
+cd ..
